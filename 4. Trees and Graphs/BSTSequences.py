487603dcc5fce8d7a910b85d049f009a3671fb85
Tommy Do <tommy.td89@gmail.com>
2019-03-20 06:29:45 -0400

    BSTSequences part 1: algorithm for all permutations of a list

---
+++ 4. Trees and Graphs/BSTSequences.py
@@ -0,0 +1,36 @@
+# A binary search tree was created by traversing through an array from left to right
+# and inserting each element. Given a binary search tree with distinct elements, print all possible
+# arrays that could have led to this tree. 
+
+# def BSTSequences(tree):
+	
+
+def permutation(lst): 
+  
+    # If lst is empty then there are no permutations 
+    if len(lst) == 0: 
+        return [] 
+  
+    # If there is only one element in lst then, only 
+    # one permuatation is possible 
+    if len(lst) == 1: 
+        return [lst] 
+  
+    # Find the permutations for lst if there are 
+    # more than 1 characters 
+  
+    l = [] # empty list that will store current permutation 
+  
+    # Iterate the input(lst) and calculate the permutation 
+    for i in range(len(lst)): 
+       m = lst[i] 
+  
+       # Extract lst[i] or m from the list.  remLst is 
+       # remaining list 
+       remLst = lst[:i] + lst[i+1:] 
+  
+       # Generating all permutations where m is first 
+       # element 
+       for p in permutation(remLst): 
+           l.append([m] + p) 
+    return l 
