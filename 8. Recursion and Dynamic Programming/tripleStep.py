3529ab683ec551d112c1dc273b16c5a6dfae556f
Tommy Do <tommy.td89@gmail.com>
2019-04-23 05:15:05 -0400

    improved efficiency of tripleStep by using memoization

--- 8. Recursion and Dynamic Programming/tripleStep.py
+++ 8. Recursion and Dynamic Programming/tripleStep.py
@@ -2,9 +2,15 @@
 # steps at a time. Implement a method to count how many possible ways the child can run up the
 # stairs. 
 
-def tripleStep(stair_length):
+def tripleStep(stair_length, stored_results):
 	if stair_length == 0:
 		return 1
 	if stair_length < 0:
 		return 0
-	return tripleStep(stair_length - 1) + tripleStep(stair_length - 2) + tripleStep(stair_length - 3)
+	if stair_length in stored_results: # checks the dictionary to see if we already have the stored answer for this recursive call
+		return stored_results[stair_length]
+	one_step = tripleStep(stair_length - 1, stored_results)
+	two_step = tripleStep(stair_length - 2, stored_results)
+	three_step = tripleStep(stair_length - 3, stored_results)
+	stored_results[stair_length] = one_step + two_step + three_step # store the solution to the recursive call so that further identical recursive calls will be solved with a O(1) look up
+	return one_step + two_step + three_step
