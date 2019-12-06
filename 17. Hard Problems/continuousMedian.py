e34029c518e5b74f575b94f1cada714528cfe264
Tommy Do <tommy.td89@gmail.com>
2019-12-06 05:04:04 -0500

    comments for continuousMedian

--- 17. Hard Problems/continuousMedian.py
+++ 17. Hard Problems/continuousMedian.py
@@ -1,6 +1,11 @@
 # Numbers are randomly generated and passed to a method. Write a program
 # to find and maintain the median value as new values are generated.
 
+
+# important concept: If we have a two heaps where the max heap contains all numbers less than the median and the min heap contains all numbers greater
+# then the median then the average of the root of the two heaps will be the median value if they are the same size. If they are not equal size, then
+# it is just the root of the max heap so long as we make the effort to always keep the max heap larger 
+
 class continuousMedian:
 
 	def __init__(self):
@@ -10,26 +15,26 @@ class continuousMedian:
 
 
 	def insert(self, number): 
-		if self.median == None:
+		if self.median == None: # initialization on first insert
 			self.insertMaxHeap(number)
 			self.median = number
-		elif number <= self.median:
-			if not self.sameSize():
+		elif number <= self.median: # insert on max heap if number is less than median
+			if not self.sameSize(): # if not same size, that means the max heap is larger by 1 so we need to balance it out before inserting an element into it
 				removed = self.max_heap[0]
 				self.insertMinHeap(removed)
 				self.max_heap = self.max_heap[1:]
-			self.insertMaxHeap(number)
-		else:
-			if self.sameSize():
+			self.insertMaxHeap(number) # if they are same size, we can just insert into max heap 
+		else: # insert on min heap if number is greater than median
+			if self.sameSize(): # if they are the same size, we must move the root of the min heap to the max heap so that when we insert into the min heap the max heap size will be larger by 1 and therefore contain the median
 				removed = self.min_heap[0]
 				self.insertMaxHeap(removed)
 				self.min_heap = self.min_heap[1:]
 			self.insertMinHeap(number)
-		self.updateMedian()
+		self.updateMedian() # after we insert we must update the median 
 
 
 
-	def insertMaxHeap(self, number):
+	def insertMaxHeap(self, number): # inserts a number into the max heap
 		self.max_heap.append(number)
 		curr_index = len(self.max_heap) - 1
 		parent = self.getParent(curr_index, self.max_heap)
@@ -38,7 +43,7 @@ class continuousMedian:
 			curr_index = (curr_index + 1) // 2 - 1
 			parent = self.getParent(curr_index, self.max_heap)
 
-	def insertMinHeap(self, number):
+	def insertMinHeap(self, number): # inserts a number into min heap
 		self.min_heap.append(number)
 		curr_index = len(self.min_heap) - 1
 		parent = self.getParent(curr_index, self.min_heap)
@@ -47,11 +52,11 @@ class continuousMedian:
 			curr_index = (curr_index + 1) // 2 - 1
 			parent = self.getParent(curr_index, self.min_heap)
 
-	def getParent(self, index, heap):
+	def getParent(self, index, heap): # gets the value of the parent of the current node
 		if index > 0:
 			return heap[(index + 1) // 2 - 1]
 
-	def swap(self, index, heap):
+	def swap(self, index, heap): # swaps the current node with its parent node
 		parent_index = (index + 1) // 2 - 1
 		temp = heap[parent_index]
 		heap[parent_index] = heap[index]
